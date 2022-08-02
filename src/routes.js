619962bf1d103eb3d9e28a5d30f33735b8d6058e
Gabriel S. Gama <77582397+gabrielbrabo@users.noreply.github.com>
2022-08-02 19:14:49 -0300

    ...

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import { Router } from "express"
 import UsersController from "./controllers/UsersController"
 import SessionsController from "./controllers/SessionsController"
 import PatientsController from "./controllers/PatientsController"
-import auth from "./middlewares/auth"
 //import auth from "./middlewares/auth"
 
 const routes = new Router()
 
 routes.post('/sessions', SessionsController.create)
 
-routes.use(auth)
 
 routes.post('/users', UsersController.create)
 routes.put('/users/:id', UsersController.update)
